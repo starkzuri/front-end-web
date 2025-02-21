35975153ea6496670ecb2a7230258d8905b48e5a
orarojack <jackoraro0@gmail.com>
2025-02-21 18:46:59 +0300

    Adjustments on some pages

--- client/src/components/comment/CommentContainer.jsx
+++ client/src/components/comment/CommentContainer.jsx
@@ -1,16 +1,9 @@
 import React, { useEffect, useState } from "react";
 import styles from "./CommentContainer.module.css";
-import profile1 from "../../assets/crystals.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faListDots,
-  faNavicon,
-  faCommentDots,
-  faSmile,
-} from "@fortawesome/free-solid-svg-icons";
-import InnerThread from "./InnerThread";
+import { faCommentDots, faSmile } from "@fortawesome/free-solid-svg-icons";
 import { useAppContext } from "../../providers/AppProvider";
-import { bigintToLongAddress, bigintToShortStr } from "../../utils/AppUtils";
+import { bigintToShortStr } from "../../utils/AppUtils";
 
 const CommentContainer = ({
   containsThread,
@@ -21,11 +14,14 @@ const CommentContainer = ({
   content,
   likes,
   postId,
-  replies,
+  replies = [],
 }) => {
   const { contract } = useAppContext();
   const [user, setUser] = useState({});
   const [loading, setLoading] = useState(false);
+  const [replyText, setReplyText] = useState("");
+  const [replyList, setReplyList] = useState(replies);
+  const [showReplyInput, setShowReplyInput] = useState(false);
 
   useEffect(() => {
     const view_user = () => {
@@ -37,7 +33,6 @@ const CommentContainer = ({
       })
         .then((res) => {
           let val = contract.callData.parse("view_user", res?.result ?? res);
-          console.log(val);
           setUser(val);
         })
         .catch((err) => {
@@ -53,6 +48,14 @@ const CommentContainer = ({
     }
   }, [contract]);
 
+  const handleReplySubmit = () => {
+    if (replyText.trim() !== "") {
+      setReplyList([...replyList, { username, content: replyText, time_commented: "Just now" }]);
+      setReplyText("");
+      setShowReplyInput(false);
+    }
+  };
+
   return (
     <div className={styles.comment_container}>
       {user && (
@@ -60,32 +63,61 @@ const CommentContainer = ({
           <div className={styles.commenter_profile_and_name}>
             <div
               className={styles.commenter_profile}
-              style={{
-                backgroundImage: `url(${user.profile_pic})`,
-              }}
+              style={{ backgroundImage: `url(${user.profile_pic})` }}
             ></div>
             <div className={styles.commenter_name}>
-              <span>{bigintToShortStr(user.username)}</span>&nbsp;
+              <span>{bigintToShortStr(user.username)}</span>
               <span className={styles.duration}>{time_commented}</span>
             </div>
           </div>
-
-          {/* <div className={styles.comment_menu}>
           <FontAwesomeIcon icon={faCommentDots} />
-        </div> */}
         </div>
       )}
+
       <div className={styles.comment_body}>
-        <p>{content && content}</p>
-        {containsThread == true && <InnerThread />}
+        <p>{content}</p>
       </div>
-      {/* <div className={styles.comment_footer}>
+
+      <div className={styles.comment_footer}>
         <button className={styles.react_button}>
           {likes && likes.toString()}
           <FontAwesomeIcon icon={faSmile} />
         </button>
-        <button className={styles.reply_button}>reply</button>
-      </div> */}
+        <button className={styles.reply_button} onClick={() => setShowReplyInput(!showReplyInput)}>
+          Reply
+        </button>
+      </div>
+
+      {showReplyInput && (
+        <div>
+          <input
+            type="text"
+            className={styles.reply_input}
+            placeholder="Write a reply..."
+            value={replyText}
+            onChange={(e) => setReplyText(e.target.value)}
+          />
+          <button className={styles.reply_button} onClick={handleReplySubmit}>
+            Submit
+          </button>
+        </div>
+      )}
+
+      {replyList.length > 0 && (
+        <div className={styles.replies_section}>
+          {replyList.map((reply, index) => (
+            <CommentContainer
+              key={index}
+              username={reply.username}
+              time_commented={reply.time_commented}
+              content={reply.content}
+              profilePic={profilePic}
+              likes={0}
+              containsThread={false}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
